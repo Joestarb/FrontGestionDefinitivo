bf5a992f037d27275d5e7ba88863e7a2dec7c0df
Arbey DzIb <120668243+Joestarb@users.noreply.github.com>
2024-02-09 11:12:48 -0500

    rutas arbey

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Login from "./pages/Login/Login"
 import Proyectos from "./pages/Admin/Proyectos/Proyectos"
 import SideBar from "./components/SideBar"
 import Footer from './components/Footer';
-import Navbar from './components/Navbar';
 import cerrar from './assets/Sidebar/cerrar.svg'
 import proyectos from './assets/Sidebar/proyectos.svg'
 import Tareas from "./pages/Miembro/Tareas/Tareas"
+import  SideBarLider from "./components/Lider/SidebarLider"
+import Inicio from './pages/Lider/Inicio';
 
 
 function App() {
@@ -17,60 +18,75 @@ function App() {
   const cerrarSesion = () => {
     sessionStorage.clear()
     localStorage.clear()
-  } 
+  }
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        
+
         <Route
           path="/administrador/*"
           element={
             <div>
               <SideBar />
               <div className='flex '>
-              <div className="flex border-r-[#cccccc] border h-[84vh] w-[5.1vw] flex-col justify-start py-[1.5%] gap-[10%] items-center">
-                <img className='w-[50%]' src={proyectos}/>
-                <Link to="/">
-                  <img className='w-[100%]' src={cerrar} onClick={cerrarSesion}/>
-                </Link>
-              </div>
-  
-                  <Routes>
-                    <Route path="proyectos" element={<Proyectos />} />
-                  </Routes>
+                <div className="flex border-r-[#cccccc] border h-[84vh] w-[5.1vw] flex-col justify-start py-[1.5%] gap-[10%] items-center">
+                  <img className='w-[50%]' src={proyectos} />
+                  <Link to="/">
+                    <img className='w-[100%]' src={cerrar} onClick={cerrarSesion} />
+                  </Link>
+                </div>
+
+                <Routes>
+                  <Route path="proyectos" element={<Proyectos />} />
+                </Routes>
               </div>
 
-              <Footer/>
+              <Footer />
 
             </div>
           }
         />
         <Route path="/miembro/*" element={
-        
-        
-        <div>
-              <SideBar />
-              <div className='flex '>
+
+
+          <div>
+            <SideBar />
+            <div className='flex '>
               <div className="flex border-r-[#cccccc] border h-[84vh] w-[5.1vw] flex-col justify-start py-[1.5%] gap-[10%] items-center">
-                <img className='w-[50%]' src={proyectos}/>
+                <img className='w-[50%]' src={proyectos} />
                 <Link to="/">
-                  <img className='w-[100%]' src={cerrar} onClick={cerrarSesion}/>
+                  <img className='w-[100%]' src={cerrar} onClick={cerrarSesion} />
                 </Link>
               </div>
-  
-                  <Routes>
-                  <Route path="tareas" element={<Tareas />} />
-                  </Routes>
-              </div>
 
-              <Footer/>
-
+              <Routes>
+                <Route path="tareas" element={<Tareas />} />
+              </Routes>
             </div>
-        
-        
+
+            <Footer />
+
+          </div>
+
+
         } />
+        <Route
+          path="/Lider/*"
+          element={
+            <div className='flex flex-row'>
+              <SideBarLider />
+              <div className='  mt-24  w-full'>
+                <Routes>
+                  <Route path="/proyectos" element={<Inicio />} />
+
+                </Routes>
+              </div>
+              <Footer />
+            </div>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
@@ -80,7 +96,7 @@ function MemberRoutes() {
   return (
     <div className='mt-16 w-full'>
       <Routes>
-        
+
       </Routes>
     </div>
   );
