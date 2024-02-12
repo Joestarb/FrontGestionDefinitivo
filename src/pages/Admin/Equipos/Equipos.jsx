77db8350d26f1b6095a1fd00a8e6bba1f85bbee3
Arbey DzIb <120668243+Joestarb@users.noreply.github.com>
2024-02-11 19:28:39 -0500

    get y delete de equipos

--- src/pages/Admin/Equipos/Equipos.jsx
+++ src/pages/Admin/Equipos/Equipos.jsx
@@ -5,22 +5,36 @@ const Equipos = () => {
   const [equipos, setEquipos] = useState([]);
   const [proyectos, setProyectos] = useState([]);
 
-  
-
   useEffect(() => {
     fetch('https://localhost:8080/equipo')
       .then(response => response.json())
       .then(data => setEquipos(data))
       .catch(error => console.error('Error fetching equipos:', error));
-      
-      fetch('https://localhost:8080/proyecto')
+
+    fetch('https://localhost:8080/proyecto')
       .then(response => response.json())
       .then(data => setProyectos(data))
       .catch(error => console.error('Error fetching proyectos:', error));
   }, []);
+
   const getNombreProyecto = (idProyecto) => {
     const proyecto = proyectos.find(proyecto => proyecto.id_proyecto === idProyecto);
     return proyecto ? proyecto.nombre : 'No encontrado';
+  };
+
+  const handleDeleteEquipo = (equipoId) => {
+    fetch(`https://localhost:8080/equipo/${equipoId}`, {
+      method: 'DELETE'
+    })
+      .then(response => {
+        if (response.ok) {
+          setEquipos(equipos.filter(equipo => equipo.id_equipo !== equipoId));
+          console.log('Equipo eliminado exitosamente');
+        } else {
+          console.error('Error al eliminar equipo');
+        }
+      })
+      .catch(error => console.error('Error al eliminar equipo:', error));
   };
 
   return (
@@ -49,26 +63,35 @@ const Equipos = () => {
 
         <div className="mt-6">
           <h2 className="text-2xl font-semibold mb-2">Equipos</h2>
-          <table className="table-auto w-full">
+          {equipos.length > 0? 
+          (<>
+              <table className="w-full">
             <thead>
               <tr>
-                <th className="border-b-2 px-4 py-2 text-sm">ID Equipo</th>
-                <th className="border-b-2 px-4 py-2 text-sm">Nombre</th>
-                <th className="border-b-2 px-4 py-2 text-sm">ID Proyecto</th>
-                <th className="border-b-2 px-4 py-2 text-sm">Acciones</th>
+                <th className="border border-gray-400 px-4 py-2">ID Equipo</th>
+                <th className="border border-gray-400 px-4 py-2">Nombre</th>
+                <th className="border border-gray-400 px-4 py-2">Nombre Proyecto</th>
+                <th className="border border-gray-400 px-4 py-2">Acciones</th>
               </tr>
             </thead>
             <tbody>
               {equipos.map((equipo, index) => (
                 <tr key={index}>
-                  <td className="border-b-2  pl-28 text-sm">{equipo.id_equipo}</td>
-                  <td className="border-b-2  pl-32 text-sm">{equipo.nombre}</td>
+                  <td className="border border-gray-400 px-4 py-2">{equipo.id_equipo}</td>
+                  <td className="border border-gray-400 px-4 py-2">{equipo.nombre}</td>
                   <td className="border border-gray-400 px-4 py-2">{getNombreProyecto(equipo.fk_proyecto)}</td>
-                  <td className="border border-gray-400 px-4 py-2">Acciones</td>
+                  <td className="border border-gray-400 px-4 py-2">
+                    <button onClick={() => handleDeleteEquipo(equipo.id_equipo)} className="bg-red-500 text-white px-2 py-1 rounded-md">Eliminar</button>
+                  </td>
                 </tr>
               ))}
             </tbody>
           </table>
+          </>):
+          (<>
+          No hay equipos añada alguno
+          </>)}
+      
         </div>
       </div>
     </div>
